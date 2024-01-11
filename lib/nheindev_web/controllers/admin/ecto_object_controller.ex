22d2c85394801bb82054ce134fc4b2232cc9d77b
nheingit <60185486+nheingit@users.noreply.github.com>
2024-01-11 14:38:25 -0600

    added view to posts table

--- lib/nheindev_web/controllers/admin/ecto_object_controller.ex
+++ lib/nheindev_web/controllers/admin/ecto_object_controller.ex
@@ -1,9 +1,16 @@
 defmodule NheindevWeb.Admin.EctoObjectController do
   use NheindevWeb, :controller
+
+  alias Nheindev.Repo
+
   def show(conn, %{"ecto_object" => ecto_object}) do
     # Here, use ecto_object to query the database for the corresponding records
     # You'll need to implement logic to handle different object types
-    posts = Nheindev.Repo.all(Nheindev.Blog.Post)
-    render(conn, "#{ecto_object}.html", records: posts)
+    records = fetch_records_for(ecto_object)
+    render(conn, "#{ecto_object}.html", records: records)
+  end
+
+  defp fetch_records_for("posts") do
+    Repo.all(Nheindev.Blog.Post)
   end
 end
