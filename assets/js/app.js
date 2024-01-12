9f3bcd5b4f83ec8fa5fbd6b867f10fbea735c404
nheingit <60185486+nheingit@users.noreply.github.com>
2024-01-12 00:05:00 -0600

    added editing functionality in the editor

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,27 +16,60 @@
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import "phoenix_html";
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
-import EditorJS from '@editorjs/editorjs'
-import Header from '@editorjs/header';
-import Paragraph from '@editorjs/paragraph';
+import { Socket } from "phoenix";
+import { LiveSocket } from "phoenix_live_view";
+import topbar from "../vendor/topbar";
+import EditorJS from "@editorjs/editorjs";
+import Header from "@editorjs/header";
+import Paragraph from "@editorjs/paragraph";
+import InlineCode from "@editorjs/inline-code";
+import RawTool from "@editorjs/raw";
+import editorjsCodecup from "@calumk/editorjs-codecup";
+
 // Import other tools as needed
 
-
 // Editor JS config
-let Hooks = {}
+let Hooks = {};
 Hooks.Editor = {
-    mounted() {
+  mounted() {
+    let content = JSON.parse(this.el.dataset.content || {})
+    this.initializeEditor(content);
+
+    let saveButton = document.querySelector("#save-editor-content");
+    saveButton.addEventListener("click", () => {
+      this.editor
+        .save()
+        .then((outputData) => {
+          const title = document.querySelector("#blog-title").value;
+          this.pushEvent("save_content", { title: title, value: outputData });
+        })
+        .catch((error) => {
+          console.log("Saving failed: ", error);
+        });
+    });
+  },
+  destroyed() {
+    if (this.editor) {
+      this.editor.destroy();
+    }
+  },
+  initializeEditor: async function (content) {
+    try {
       this.editor = new EditorJS({
-        holder: 'editorjs',
+        holder: "editorjs",
+        data: content,
         tools: {
+          code: editorjsCodecup,
+          raw: RawTool,
           header: {
             class: Header,
             inlineToolbar: true,
+          },
+          inlineCode: {
+            class: InlineCode,
+            shortcut: "CMD+SHIFT+M",
           },
           paragraph: {
             class: Paragraph,
@@ -44,39 +77,33 @@ Hooks.Editor = {
           },
         },
         onReady: () => {
-          console.log('Editor.js is ready to work!')
-        }
+          console.log("Editor.js is ready to work!");
+        },
       });
-    let saveButton = document.querySelector("#save-editor-content");
-    saveButton.addEventListener("click", () => {
-      this.editor.save().then((outputData) => {
-        const title = document.querySelector("#blog-title").value;
-        this.pushEvent("save_content", {title: title, value: outputData});
-      }).catch((error) => {
-        console.log('Saving failed: ', error);
-      });
-    });
-  },
-  destroyed() {
-    if (this.editor) {
-      this.editor.destroy();
+    } catch (error) {
+      console.log("Error initializing Editor.js:", error);
     }
-  }
-}
+  },
+};
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  hooks: Hooks,
+  params: { _csrf_token: csrfToken },
+});
 
 // Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
-window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+window.addEventListener("phx:page-loading-start", (_info) => topbar.show(300));
+window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
